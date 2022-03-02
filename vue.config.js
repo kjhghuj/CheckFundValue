320e0b58d01a235306508e99eb9ab125707f0199
Gsy <kjhghuj@icloud.com>
2022-03-02 21:53:52 +0800

    核酸

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     // lintOnSave: false, //是否开启eslint
     devServer: {
         open: true, //是否自动弹出浏览器页面
-        host: "localhost", //表示启动的时候使用的域名，默认可以不写，则是使用localhost和本机IP
+        // host: "localhost", //表示启动的时候使用的域名，默认可以不写，则是使用localhost和本机IP
         port: '8081', // 设置端口号
         https: false,  //是否使用https协议
         hotOnly: true, //是否开启热更新
@@ -57,7 +57,7 @@ module.exports = {
               filename: '[path].gz[query]', // 压缩后的文件名(保持原文件名，后缀加.gz)
               minRatio: 1, // 压缩率小于1才会压缩
               threshold: 10240, // 对超过10k的数据压缩
-              deleteOriginalAssets: false, // 是否删除未压缩的源文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false（比如删除打包后的gz后还可以加载到原始资源文件）
+              deleteOriginalAssets: true, // 是否删除未压缩的源文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false（比如删除打包后的gz后还可以加载到原始资源文件）
           })
         )
       }
